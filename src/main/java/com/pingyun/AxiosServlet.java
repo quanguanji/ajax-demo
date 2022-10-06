34b225312016d43336be1d3c367a2dd246fe896d
jackson <2954395020@qq.com>
2022-10-06 12:32:22 +0800

    init ajax-demo

---
+++ src/main/java/com/pingyun/AxiosServlet.java
@@ -0,0 +1,21 @@
+package com.pingyun;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/axiosServlet")
+public class AxiosServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        System.out.println(username);
+        response.getWriter().write("hello axios");
+    }
+}
